198e3b47fbc8219ab68479d1c559d4aea1957964
Martin Taylor <martin6taylor@gmail.com>
2014-02-28 20:29:46 +0800

    add SDL support

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,19 @@
 
 #include <GL/gl.h>
 #include <GL/glu.h>
+
+#ifdef USE_GLUT
 #include <GL/glut.h>
+#else
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#endif
 
 const int FRAMES_PER_SECOND=60;
 static const unsigned int interval=750; // milliseconds
 Engine engine;
+
+#ifdef USE_GLUT
 
 void display()
 {
@@ -59,6 +67,28 @@ void keyboard(unsigned char key, int x, int y)
 	}
 }
 
+#else
+
+SDL_Window* window=NULL;
+Uint32 timer(Uint32 interval, void* param)
+{
+	engine.update();
+	glClear(GL_COLOR_BUFFER_BIT);
+	engine.draw();
+	
+	return interval;
+}
+
+void render()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	engine.draw();
+}
+
+#endif /* USE_GLUT */
+
+
+
 bool initGL(int width, int height)
 {
 	GLenum error=GL_NO_ERROR;
@@ -86,6 +116,7 @@ int main( int argc, char **argv )
 	int width =Grid::WIDTH *engine.getGridWidth();
 	int height=Grid::HEIGHT*engine.getGridWidth();
 
+#ifdef USE_GLUT
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
 
@@ -101,6 +132,76 @@ int main( int argc, char **argv )
 	glutSpecialFunc(special);
 	glutKeyboardFunc(keyboard);
 	glutMainLoop();
+
+#else
+
+	SDL_GLContext context;
+    if(SDL_Init(SDL_INIT_EVERYTHING)!=0)
+	{
+		printf("Unable to initialize SDL: %s\n", SDL_GetError());;
+    	SDL_Quit();
+    	return 1;
+	}
+	else
+	{
+/*
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+		SDL_GL_SetAttribute(SDL_GL_SINGLE_BUFFER, 1);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+*/
+		window=SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+			width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_MOUSE_FOCUS);
+		if(!window)
+		{
+			printf("Unable to create window: %s\n", SDL_GetError());
+			SDL_Quit();
+			return 2;
+		}
+		
+		context=SDL_GL_CreateContext(window);
+		SDL_GL_SetSwapInterval(1);
+
+		if(!initGL(width, height))
+		{
+			printf("OpenGL initialized error!\n");
+			SDL_GL_DeleteContext(context);
+			SDL_DestroyWindow(window);
+			return 3;
+		}
+	}
+	
+	SDL_AddTimer(interval, timer, NULL);
+	
+	SDL_Event event;
+	bool quit=false;
+	while(!quit)
+	{
+		SDL_PollEvent(&event);
+		
+		if(event.type==SDL_QUIT)
+			break;
+		else if(event.type==SDL_KEYDOWN)
+		{
+			switch(event.key.keysym.sym)
+			{
+				case SDLK_LEFT:	 engine.handleKey(Engine::LEFT);  break;
+				case SDLK_RIGHT: engine.handleKey(Engine::RIGHT); break;
+				case SDLK_DOWN:  engine.handleKey(Engine::DOWN);  break;
+				case SDLK_UP:    engine.handleKey(Engine::SHIFT); break;
+				case SDLK_ESCAPE: quit=true; break;
+			}
+		}
+		
+		glClear(GL_COLOR_BUFFER_BIT);
+		engine.draw();
+		SDL_GL_SwapWindow(window);
+	}
+	
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
+	
+#endif /* USE_GLUT */
 
 	return 0;
 }
